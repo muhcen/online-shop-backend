ddc2768ca57b36d394268ddd11a74e754d68833e
mohsen <mohsenun7@gmail.com>
2021-06-28 15:57:21 +0430

    fixed some bug

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,14 @@ const User = require('./../models/userModel');
 const errorHandler = require('./errorController');
 const jwt = require('jsonwebtoken');
 
-const createJWT = (res, email) => {
-    const token = jwt.sign(email, process.env.SECRET_JWT_KEY);
-    res.cookie('jwt', token, { maxAge: process.env.EXPIRE_JWT, httponly: true });
+const createJWT = (res, id) => {
+    const token = jwt.sign({ id }, process.env.SECRET_JWT_KEY, {
+        expiresIn: process.env.EXPIRE_IN,
+    });
+    res.cookie('jwt', token, {
+        expiresIn: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+        httponly: true,
+    });
     return token;
 };
 
@@ -13,7 +18,7 @@ exports.registir = async (req, res) => {
         const { email, password, confirmPassword, name, phone } = req.body;
         const user = await User.create({ email, name, password, confirmPassword, phone });
 
-        const jwt = createJWT(res, user.email);
+        const jwt = createJWT(res, user.id);
         res.status(200).json({
             status: 'success',
             jwt,
@@ -36,14 +41,13 @@ exports.login = async (req, res) => {
         return errorHandler(res, 400, 'plz, input email and password');
     }
 
-    const user = await User.findOne({ email });
     console.log(await user.validatePassword(password));
 
     if (!user || !(await user?.validatePassword(password))) {
         return errorHandler(res, 400, 'email or password is not correct');
     }
 
-    const jwt = createJWT(res, user.email);
+    const jwt = createJWT(res, user.id);
     console.log(res.cookie.name);
     res.status(200).json({
         status: 'success',
