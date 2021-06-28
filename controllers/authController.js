9663538af3e5ab080fff00b4ed87cbfcdeb713f0
mohsen <mohsenun7@gmail.com>
2021-06-28 18:56:30 +0430

    protect route

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,16 +41,42 @@ exports.login = async (req, res) => {
         return errorHandler(res, 400, 'plz, input email and password');
     }
 
-    console.log(await user.validatePassword(password));
+    const user = await User.findOne({ email });
 
     if (!user || !(await user?.validatePassword(password))) {
         return errorHandler(res, 400, 'email or password is not correct');
     }
 
     const jwt = createJWT(res, user.id);
-    console.log(res.cookie.name);
+
     res.status(200).json({
         status: 'success',
         jwt,
     });
+};
+
+exports.protect = async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.jwt) {
+        token = req.cookies.jwt;
+    }
+
+    if (!token) {
+        return errorHandler(res, 400, 'first login after that you can access this route');
+    }
+
+    const obj = await jwt.verify(token, process.env.SECRET_JWT_KEY);
+
+    const user = await User.findById(obj.id);
+
+    if (!user) {
+        return errorHandler(res, 400, 'token is not valid');
+    }
+
+    req.user = user;
+    res.locals.user = user;
+    next();
 };
