bbc684c58c21b9c9afa25d4f5b344896fad4dc58
mohsen <mohsenun7@gmail.com>
2021-06-30 12:21:38 +0430

    create goods route

--- controllers/goodsController.js
+++ controllers/goodsController.js
@@ -1,3 +1,20 @@
 const Goods = require('./../models/goodsModel');
+const errorHandler = require('./errorController');
 
-exports.createGoods = async (req, res) => {};
+exports.createGoods = async (req, res) => {
+    try {
+        const good = await Goods.create(req.body);
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                good,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message,
+        });
+    }
+};
